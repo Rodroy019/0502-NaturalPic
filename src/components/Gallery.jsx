46260324902c6869d344e29228914642170b8705
Rodrigo Rivas <rodroy019@hotmail.com>
2024-04-14 23:01:15 -0400

    add: se agrega la funcion addAndRemove

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,24 @@
 import Card from 'react-bootstrap/Card'
 import IconHeart from './IconHeart'
+import { useContext } from 'react'
+import { LikeMe } from '../context/LikeMe'
 
 const Gallery = ({ image }) => {
+  const { images, setImages } = useContext(LikeMe)
+
+  const addAndRemove = (id) => {
+    const newImages = images.map(image => {
+      if (image.id === id) {
+        return {
+          ...image,
+          liked: !image.liked
+        }
+      }
+      return image
+    })
+    setImages(newImages)
+  }
+
   return (
     <div className='col-md-4 mb-4'>
       <Card className='text-white'>
@@ -11,7 +28,7 @@ const Gallery = ({ image }) => {
             <div className='text-container'>
               <p className='image-text'>{image.alt}</p>
             </div>
-            <button className='transparent-button'>
+            <button className='transparent-button' onClick={() => addAndRemove(image.id)}>
               <IconHeart filled={image.liked} />
             </button>
           </Card.ImgOverlay>
