28f40dfa7242a606df0f42078b018b36abfd06cf
Rodrigo Rivas <rodroy019@hotmail.com>
2024-04-14 22:45:39 -0400

    change: se pasan los style a css y se agrega boton a like con icono de corazon

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,14 +5,16 @@ const Gallery = ({ image }) => {
   return (
     <div className='col-md-4 mb-4'>
       <Card className='text-white'>
-        <div style={{ position: 'relative', width: '350px', height: '300px', overflow: 'hidden' }}>
+        <div className='gallery-container'>
           <Card.Img src={image.src.large} alt={image.alt} className='img-fluid' />
-          <div style={{ position: 'absolute', bottom: '0', left: '0', padding: '10px', width: '100%' }}>
-            <p style={{ color: 'white', marginBottom: '0', fontSize: '14px' }}>{image.alt}</p>
-            <div style={{ position: 'absolute', bottom: '150px', right: '10px' }}>
-              <IconHeart filled={image.liked} />
+          <Card.ImgOverlay>
+            <div className='text-container'>
+              <p className='image-text'>{image.alt}</p>
             </div>
-          </div>
+            <button className='transparent-button'>
+              <IconHeart filled={image.liked} />
+            </button>
+          </Card.ImgOverlay>
         </div>
       </Card>
     </div>
