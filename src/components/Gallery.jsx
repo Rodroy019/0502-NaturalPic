9b7083823e8e79c75941657ddcbcc745842aea75
Rodrigo Rivas <rodroy019@hotmail.com>
2024-04-14 22:20:42 -0400

    add: se agrega diseño, texto queda abajo a la izq y corazon arriba a la derecha

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,15 +5,15 @@ const Gallery = ({ image }) => {
   return (
     <div className='col-md-4 mb-4'>
       <Card className='text-white'>
-        <div style={{ width: '350px', height: '300px', overflow: 'hidden' }}>
+        <div style={{ position: 'relative', width: '350px', height: '300px', overflow: 'hidden' }}>
           <Card.Img src={image.src.large} alt={image.alt} className='img-fluid' />
+          <div style={{ position: 'absolute', bottom: '0', left: '0', padding: '10px', width: '100%' }}>
+            <p style={{ color: 'white', marginBottom: '0', fontSize: '14px' }}>{image.alt}</p>
+            <div style={{ position: 'absolute', bottom: '150px', right: '10px' }}>
+              <IconHeart filled={image.liked} />
+            </div>
+          </div>
         </div>
-        <Card.ImgOverlay>
-          <Card.Text>
-            {image.alt}
-          </Card.Text>
-          <IconHeart filled={image.liked} />
-        </Card.ImgOverlay>
       </Card>
     </div>
   )
